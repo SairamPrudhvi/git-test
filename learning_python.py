9b23ad23620296e9a23f83644d9da6a7257df6f8
Sairam Prudhvi <ramprudhvi.sp@gmail.com>
2026-01-12 18:44:32 -0500

    added python code

---
+++ learning_python.py
@@ -0,0 +1,35 @@
+# This program prints a greeting
+print('Hello World!!', end = '\n')
+
+"""
+Exercise: write a multi-line comment
+Explain what this program does
+
+"""
+print("Python is easy to learn.", end = '\n')
+
+# Exercise: create variable for your name and age, then print them
+name = 'Sairam Prudhvi'
+age = 27
+
+print(f'My name is {name} and Im {age} years old.', end = '\n')
+
+# Create the variable for two numbers and print them.
+num1, num2 = 10, 5
+print(num1 + num2)
+
+#artihmetic operator
+
+a, b = 10, 3
+print(a + b)
+print(a * b)
+print(a - b)
+print(a / b)
+print(a ** b)
+
+
+x = 5
+print(x == 5)
+print(x != 5)
+print(x > 3)
+print(x < 3)
